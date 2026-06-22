553c51ab1b6af14ac5366e8a0b6da58548c0c45d
GustavoTamaninis <194152443+GustavoTamaninis@users.noreply.github.com>
2026-06-22 17:35:36 -0300

    feat: this e parâmetro tipo referência

--- src/com/java/devdojo/javacore/introducaoclasses/dominio/Estudante.java
+++ src/com/java/devdojo/javacore/introducaoclasses/dominio/Estudante.java
@@ -3,4 +3,10 @@ package com.java.devdojo.javacore.introducaoclasses.dominio;
 public class Estudante {
     public String nome;
     public int idade;
+
+    public void imprime(){
+        this.nome = "Yan";
+        System.out.println("Nome atual do estudante: " + this.nome);
+        //estudante.nome = "Yan"; // perigoso fazer isso.
+    }
 }
