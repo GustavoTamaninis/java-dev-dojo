3649396bc61e3f87c790fbc25b3dfe2bccd92978
GustavoTamaninis <194152443+GustavoTamaninis@users.noreply.github.com>
2026-06-26 13:50:21 -0300

    feat: sobrecarga de métodos pt2

--- src/com/java/devdojo/sobrecargametodos/dominio/Livro.java
+++ src/com/java/devdojo/sobrecargametodos/dominio/Livro.java
@@ -4,6 +4,7 @@ public class Livro {
     private String nome;
     private String tipo;
     private int paginas;
+    private String genero;
 
     public void init(String nome, String tipo, int paginas){
         this.nome = nome;
@@ -11,10 +12,17 @@ public class Livro {
         this.paginas = paginas;
     }
 
+    //Sobrecarga de métodos: mesmo nome, parâmetros diferentes:
+    public void init(String nome, int paginas,String genero){
+        this.init(nome, tipo, paginas);
+        this.genero = genero;
+    }
+
     public void imprime(){
         System.out.println(this.nome);
         System.out.println(this.tipo);
         System.out.println(this.paginas);
+        System.out.println(this.genero);
     }
 
     public void setNome(String nome) {
@@ -28,7 +36,7 @@ public class Livro {
         this.tipo = tipo;
     }
     public String getTipo() {
-        return this.tipo;
+        return tipo;
     }
 
     public void setPaginas(int paginas) {
@@ -36,5 +44,13 @@ public class Livro {
     }
     public int getPaginas() {
         return this.paginas;
+    }
+
+    public String getGenero() {
+        return genero;
+    }
+
+    public void setGenero(String genero) {
+        this.genero = genero;
     }
 }
--- src/com/java/devdojo/sobrecargametodos/teste/LivroTeste01.java
+++ src/com/java/devdojo/sobrecargametodos/teste/LivroTeste01.java
@@ -5,8 +5,9 @@ import com.java.devdojo.sobrecargametodos.dominio.Livro;
 public class LivroTeste01 {
     public static void main(String[] args){
         Livro livro = new Livro();
-        //muito melhor do que usar vários sets:
-        livro.init("Ortodoxia", "Filosofia", 300);
+        //Muito melhor do que usar vários sets:
+        livro.init("Ortodoxia", "Físico", 300);
+        livro.init("Padre Brown", 300, "Policial");
         livro.imprime();
     }
 }
