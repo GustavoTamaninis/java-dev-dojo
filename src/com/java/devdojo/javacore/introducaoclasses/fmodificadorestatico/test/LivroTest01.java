06cbf3180c925e585ee3baf723fd2c283f6964c7
GustavoTamaninis <194152443+GustavoTamaninis@users.noreply.github.com>
2026-06-30 20:40:37 -0300

    feat: bloco de inicialização estático

---
+++ src/com/java/devdojo/javacore/introducaoclasses/fmodificadorestatico/test/LivroTest01.java
@@ -0,0 +1,11 @@
+package com.java.devdojo.javacore.introducaoclasses.fmodificadorestatico.test;
+
+import com.java.devdojo.javacore.introducaoclasses.fmodificadorestatico.domain.Livro;
+
+public class LivroTest01 {
+    static void main(String[] args) {
+        Livro livro = new Livro();
+        Livro livro2 = new Livro();
+        Livro livro3 = new Livro();
+    }
+}
