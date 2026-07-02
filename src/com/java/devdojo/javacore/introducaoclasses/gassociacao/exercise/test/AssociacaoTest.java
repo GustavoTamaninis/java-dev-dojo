ae5fc9a660a25bf1b9ad155043ef24e5facdd758
GustavoTamaninis <194152443+GustavoTamaninis@users.noreply.github.com>
2026-07-02 12:59:54 -0300

    feat: associação bidirecional main

--- src/com/java/devdojo/javacore/introducaoclasses/gassociacao/exercise/test/AssociacaoTest.java
+++ src/com/java/devdojo/javacore/introducaoclasses/gassociacao/exercise/test/AssociacaoTest.java
@@ -1,4 +1,14 @@
 package com.java.devdojo.javacore.introducaoclasses.gassociacao.exercise.test;
 
+import com.java.devdojo.javacore.introducaoclasses.gassociacao.exercise.domain.Seminario;
+
+import java.util.Scanner;
+
 public class AssociacaoTest {
+    static void main(String[] args) {
+        System.out.println("Digite o título do seminário:");
+        Scanner in = new Scanner(System.in);
+        Seminario seminario = new Seminario("A Descoberta do Brasil");
+        seminario.imprime();
+    }
 }
