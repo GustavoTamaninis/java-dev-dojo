b49a3cc1585b838cfee4b75792829a020e3017a6
GustavoTamaninis <194152443+GustavoTamaninis@users.noreply.github.com>
2026-07-03 15:48:04 -0300

    feat: super

--- src/com/java/devdojo/javacore/introducaoclasses/hheranca/domain/Manager.java
+++ src/com/java/devdojo/javacore/introducaoclasses/hheranca/domain/Manager.java
@@ -3,6 +3,12 @@ package com.java.devdojo.javacore.introducaoclasses.hheranca.domain;
 public class Manager extends Employee{
     private String department;
 
+    @Override //it's explicites a override
+    public void print(){
+        super.print(); // it calls the method print from the superclass.
+        System.out.println(department + " department");
+    }
+
     public String getDepartmentNumber() {
         return department;
     }
