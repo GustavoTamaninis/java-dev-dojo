3824fb64d90d52b676e20739db6b7e7a3fb80711
GustavoTamaninis <194152443+GustavoTamaninis@users.noreply.github.com>
2026-07-06 14:47:08 -0300

    feat: overriding toString

---
+++ src/com/java/devdojo/javacore/introducaoclasses/ioverriding/domain/MovieTest01.java
@@ -0,0 +1,9 @@
+package com.java.devdojo.javacore.introducaoclasses.ioverriding.domain;
+
+public class MovieTest01 {
+    public static void main(String[] args) {
+        Movie movie = new Movie("Expresso Polar");
+        System.out.println(movie.toString());
+    }
+
+}
