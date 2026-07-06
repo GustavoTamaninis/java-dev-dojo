cffa1dda2e5d10a688b43f2e77a769078b02e7c8
GustavoTamaninis <194152443+GustavoTamaninis@users.noreply.github.com>
2026-07-06 18:42:19 -0300

    feat: final modifier - reference types

--- src/com/java/devdojo/javacore/introducaoclasses/ioverriding/test/MovieTest01.java
+++ src/com/java/devdojo/javacore/introducaoclasses/ioverriding/test/MovieTest01.java
@@ -1,4 +1,6 @@
-package com.java.devdojo.javacore.introducaoclasses.ioverriding.domain;
+package com.java.devdojo.javacore.introducaoclasses.ioverriding.test;
+
+import com.java.devdojo.javacore.introducaoclasses.ioverriding.domain.Movie;
 
 public class MovieTest01 {
     public static void main(String[] args) {
--- src/com/java/devdojo/javacore/introducaoclasses/jfinalmodifier/domain/Car.java
+++ src/com/java/devdojo/javacore/introducaoclasses/jfinalmodifier/domain/Car.java
@@ -6,6 +6,8 @@ public class Car {
     // I can initialize the value of a static variable like this:
     public static final double SPEED_LIMIT; // if it don't have static, it cannot be called from the class,
     // but it can be initialized in a constructor and in an initialization block.
+    public final Buyer BUYER = new Buyer(); //usually, a final object is used in Singleton pattern.
+    // The reference "BUYER" cannot be changed, but the object's attributes can be changed
     static{
         SPEED_LIMIT = 240; //by the time you are done with loading this class in JVM, the value will be available to be used
     }
