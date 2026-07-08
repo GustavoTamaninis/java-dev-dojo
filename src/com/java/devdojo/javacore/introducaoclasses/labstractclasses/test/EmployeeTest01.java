e28f4c2802b0173eae810545f13bc16dd2b6fbd5
GustavoTamaninis <194152443+GustavoTamaninis@users.noreply.github.com>
2026-07-08 14:03:52 -0300

    feat: abstract methods

--- src/com/java/devdojo/javacore/introducaoclasses/labstractclasses/test/EmployeeTest01.java
+++ src/com/java/devdojo/javacore/introducaoclasses/labstractclasses/test/EmployeeTest01.java
@@ -1,5 +1,6 @@
 package com.java.devdojo.javacore.introducaoclasses.labstractclasses.test;
 
+import com.java.devdojo.javacore.introducaoclasses.labstractclasses.domain.Developer;
 import com.java.devdojo.javacore.introducaoclasses.labstractclasses.domain.Employee;
 import com.java.devdojo.javacore.introducaoclasses.labstractclasses.domain.Manager;
 
@@ -8,5 +9,8 @@ public class EmployeeTest01 {
     public static void main(String[] args) {
         Manager manager = new Manager("João", 4900);
         System.out.println(manager);
+
+        Developer developer = new Developer("Gustavo", 2700);
+        System.out.println(developer);
     }
 }
