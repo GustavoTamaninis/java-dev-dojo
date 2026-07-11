ce1e7a36289ecae5e8cb2c5274b6d572ba40b5fe
GustavoTamaninis <194152443+GustavoTamaninis@users.noreply.github.com>
2026-07-11 12:30:31 -0300

    feat: throwing unchecked exceptions

---
+++ src/com/java/devdojo/javacore/javacore/oexception/runtime/test/RuntimeExceptionTest02.java
@@ -0,0 +1,27 @@
+package com.java.devdojo.javacore.javacore.oexception.runtime.test;
+
+public class RuntimeExceptionTest02 {
+    static void main(String[] args) {
+        try{
+            System.out.println(divisao(2, 0));;
+        }catch (RuntimeException e){
+            e.printStackTrace();
+        }
+        System.out.println("Código finalizado.");
+    }
+
+    /**
+     *
+     * @param a
+     * @param b cannot be zero
+     * @return
+     * @throws IllegalArgumentException if b is zero
+     */
+    private static int divisao(int a, int b) throws IllegalArgumentException{ // it's possible too
+//    private static int divisao(int a, int b){
+        if(b == 0){
+            throw new IllegalArgumentException("Argumento ilegal, não pode ser 0."); // it's like a "break"
+        }
+        return a/b;
+    }
+}
