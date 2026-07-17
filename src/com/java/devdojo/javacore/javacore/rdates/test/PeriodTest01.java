a299fab2c531d9d334478fdacb0f63239afa80c0
GustavoTamaninis <194152443+GustavoTamaninis@users.noreply.github.com>
2026-07-17 15:24:43 -0300

    feat: chronounit

--- src/com/java/devdojo/javacore/javacore/rdates/test/PeriodTest01.java
+++ src/com/java/devdojo/javacore/javacore/rdates/test/PeriodTest01.java
@@ -2,6 +2,7 @@ package com.java.devdojo.javacore.javacore.rdates.test;
 
 import java.time.LocalDate;
 import java.time.Period;
+import java.time.temporal.ChronoUnit;
 
 public class PeriodTest01 {
     static void main(String[] args) {
@@ -11,10 +12,14 @@ public class PeriodTest01 {
         Period p1 = Period.between(now, nowAfterTwoYears); // não aceita LocalDateTime, mas só LocalDate.
         Period p2 = Period.ofWeeks(3);
         Period p3 = Period.ofMonths(3);
+        Period p4 = Period.ofDays(63);
 
         System.out.println(p1);
         System.out.println(p2); // não imprime semanas, mas dias.
         System.out.println(p3);
+
+        System.out.println(now.until(now.plusDays(p4.getDays()), ChronoUnit.MONTHS));
+        System.out.println(now.until(now.plusDays(p4.getDays()), ChronoUnit.WEEKS));
     }
 
 }
