81c53c19b932699f9481835844b6eb13b5464847
GustavoTamaninis <194152443+GustavoTamaninis@users.noreply.github.com>
2026-07-18 22:24:50 -0300

    feat: io - file writer

---
+++ src/com/java/devdojo/javacore/javacore/vio/test/FileWriterTest01.java
@@ -0,0 +1,21 @@
+package com.java.devdojo.javacore.javacore.vio.test;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class FileWriterTest01 {
+    static void main(String[] args) {
+        File file = new File("/home/gustavotama/IdeaProjects/java-dev-dojo/src/com/java/devdojo/javacore/javacore/vio/files/file01.txt");
+        try(FileWriter fw = new FileWriter(file, true)) { // Pondo esse booleano, o write não reescreve por cima, mas faz "append".
+            // Se não colocar nos parênteses do try, devo fechar isto com close(), pois essa classe é subclasse de Closeable e, portanto, lida com recursos do OS.
+            fw.write("Louvado seja Nosso Senhor Jesus Cristo! Para sempre seja louvado! E salve Maria!\n");
+            // Um buffer é um espaço temporário na memória onde são guardados dados que estão sendo transferidos de um lugar para outro.
+            // Ao fechar um arquivo, existe uma chance do OS não. Para evitar isso, use o flush:
+            fw.flush(); // quando este méthodo acabar, o próprio java vai chamar o méthodo close()
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
+}
