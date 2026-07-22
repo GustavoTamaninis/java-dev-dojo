bcd3f7a90db0ac444997243b5eaadcddca10def2
GustavoTamaninis <194152443+GustavoTamaninis@users.noreply.github.com>
2026-07-21 21:35:02 -0300

    feat: nio - simple visitor pt 1

---
+++ src/com/java/devdojo/javacore/javacore/wnio/test/SimpleFileVisitorTest01.java
@@ -0,0 +1,22 @@
+package com.java.devdojo.javacore.javacore.wnio.test;
+
+import java.io.IOException;
+import java.nio.file.*;
+import java.nio.file.attribute.BasicFileAttributes;
+
+class ListAllFiles extends SimpleFileVisitor<Path> { // todos os metodos ter o Path
+
+    @Override
+    public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) {
+        // imprimir todos os arquivos Java do repositório:
+        if(file.getFileName().toString().endsWith(".java")) System.out.println(file.getFileName());
+        return FileVisitResult.CONTINUE; // posso retornar CONTINUE, TERMINATE, SKIP_SUBTREE OU SKIP_SIBLINGS.
+    }
+}
+
+public class SimpleFileVisitorTest01 {
+    static void main(String[] args) throws IOException {
+        Path root = Paths.get(".");
+        Files.walkFileTree(root, new ListAllFiles());
+    }
+}
