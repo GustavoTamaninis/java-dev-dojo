b7d820c71d32e4441b0e0e553bd0b4f14cfc3570
GustavoTamaninis <194152443+GustavoTamaninis@users.noreply.github.com>
2026-07-29 22:34:25 -0300

    feat: list pt 2

--- src/com/java/devdojo/javacore/javacore/ycollections/test/ListaTest01.java
+++ src/com/java/devdojo/javacore/javacore/ycollections/test/ListaTest01.java
@@ -9,6 +9,8 @@ public class ListaTest01 {
         List nomes = new ArrayList(); // é uma interface. Lista ordenada.
         nomes.add("Gustavo Henrique");
         nomes.add("Ronaldo Aldo");
+        nomes.add("Sílvio Silvano");
+        nomes.remove("Ronaldo Aldo"); // posso tanto índice quanto objeto. No caso de string, é usado equals pra comparar.
         nomes.add(5423); // infelizmente mistura tipos na lista.
 
         for(Object nome: nomes){
@@ -25,6 +27,7 @@ public class ListaTest01 {
 
         for(String cidade: cidades){ // agora posso colocar String no tipo.
             System.out.println(cidade); // possível usar replace.
+//            cidades.add("Jaguaquara"); // vai dar exceção, pois o for já possui um tamanho definido.
         }
 
         System.out.println("-------------------");
@@ -34,5 +37,31 @@ public class ListaTest01 {
         for(int i = 0; i < cidades.size(); i++){
             System.out.println("Cidade de " + cidades.get(i)); // não posso usar nomes[i].
         }
+
+        System.out.println("-------------------");
+
+        // Como adicionar dentro do for sem causar loop:
+        int size = cidades.size();
+        for(int i = 0; i < size; i++){
+            cidades.add("Fortaleza");
+        }
+        System.out.println(cidades);
+
+        System.out.println("-------------------");
+
+        // Ao criar uma coleção, deve ser sempre um objeto: List<int> não funciona; List<Integer> sim.
+        // Isso ocorre porque as coleções chamam hashCode, que tipos primitivos não possuem, obviamente.
+        List<Integer> nums = new ArrayList<>();
+        nums.add(1);
+        nums.add(10);
+
+        // É possível fundir listas também:
+        List<Integer> primos = new ArrayList<>();
+        nums.add(2);
+        nums.add(3);
+        nums.add(5);
+
+        nums.addAll(primos);
+        System.out.println(nums);
     }
 }
